e0a5574dc7bddc13bceb42f907b8122950545bf2
Leandro Pereira <leandro@leandro.io>
2025-08-20 08:59:03 -0400

    remove 3rd-party single letter sigils

--- queries/injections.scm
+++ queries/injections.scm
@@ -14,14 +14,6 @@
  (#set! injection.language "regex")
  (#set! injection.combined))
 
-; Json
-((sigil
-  (sigil_name) @_sigil_name
-  (quoted_content) @injection.content)
- (#any-of? @_sigil_name "j" "J")
- (#set! injection.language "json")
- (#set! injection.combined))
-
 ; SQL injection
 ((sigil
   (sigil_name) @_sigil_name
@@ -30,28 +22,12 @@
  (#set! injection.language "sql")
  (#set! injection.combined))
 
-; Surface
-((sigil
-  (sigil_name) @_sigil_name
-  (quoted_content) @injection.content)
- (#eq? @_sigil_name "F")
- (#set! injection.language "surface")
- (#set! injection.combined))
-
 ; Markdown
 ((sigil
   (sigil_name) @_sigil_name
   (quoted_content) @injection.content)
  (#eq? @_sigil_name "MD")
  (#set! injection.language "markdown")
- (#set! injection.combined))
-
-; Zig
-((sigil
-  (sigil_name) @_sigil_name
-  (quoted_content) @injection.content)
- (#any-of? @_sigil_name "z" "Z")
- (#set! injection.language "zig")
  (#set! injection.combined))
 
 ; Python
@@ -68,14 +44,6 @@
   (quoted_content) @injection.content)
  (#eq? @_sigil_name "JS")
  (#set! injection.language "javascript")
- (#set! injection.combined))
-
-; Svelte
-((sigil
-  (sigil_name) @_sigil_name
-  (quoted_content) @injection.content)
- (#eq? @_sigil_name "V")
- (#set! injection.language "svelte")
  (#set! injection.combined))
 
 ; Vue
