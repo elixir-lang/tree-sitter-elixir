d24cecee673c4c770f797bac6f87ae4b6d7ddec5
Leandro Pereira <leandro@leandro.io>
2025-08-21 04:18:24 -0400

    feat: inject sigils (#86)

--- queries/injections.scm
+++ queries/injections.scm
@@ -2,8 +2,16 @@
 ((sigil
   (sigil_name) @_sigil_name
   (quoted_content) @injection.content)
- (#any-of? @_sigil_name "H" "LVN")
+ (#any-of? @_sigil_name "H" "LVN" "HOLO")
  (#set! injection.language "heex")
+ (#set! injection.combined))
+
+; Regex
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content)
+ (#any-of? @_sigil_name "r" "R")
+ (#set! injection.language "regex")
  (#set! injection.combined))
 
 ; SQL injection
@@ -12,4 +20,36 @@
   (quoted_content) @injection.content)
  (#eq? @_sigil_name "SQL")
  (#set! injection.language "sql")
+ (#set! injection.combined))
+
+; Markdown
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content)
+ (#eq? @_sigil_name "MD")
+ (#set! injection.language "markdown")
+ (#set! injection.combined))
+
+; Python
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content)
+ (#eq? @_sigil_name "PY")
+ (#set! injection.language "python")
+ (#set! injection.combined))
+
+; JavaScript
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content)
+ (#eq? @_sigil_name "JS")
+ (#set! injection.language "javascript")
+ (#set! injection.combined))
+
+; Vue
+((sigil
+  (sigil_name) @_sigil_name
+  (quoted_content) @injection.content)
+ (#eq? @_sigil_name "VUE")
+ (#set! injection.language "vue")
  (#set! injection.combined))
